20179d5b9198bbffe7bbb291067a6f5487ddc1ee
zekroTJA <riho@live.de>
2024-08-02 17:01:26 +0200

    update response model

--- src/model.rs
+++ src/model.rs
@@ -4,9 +4,11 @@ use serde::Serialize;
 
 #[derive(Serialize, Default)]
 pub struct CommandResult {
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub stdout: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub stderr: Option<String>,
-    pub status_code: i32,
+    pub status_code: Option<i32>,
 }
 
 impl TryFrom<Output> for CommandResult {
@@ -15,7 +17,7 @@ impl TryFrom<Output> for CommandResult {
     fn try_from(value: Output) -> Result<Self, Self::Error> {
         let stdout = Some(String::from_utf8(value.stdout)?);
         let stderr = Some(String::from_utf8(value.stderr)?);
-        let status_code = value.status.code().unwrap_or_default();
+        let status_code = value.status.code();
 
         Ok(Self {
             stdout,
@@ -28,7 +30,7 @@ impl TryFrom<Output> for CommandResult {
 impl From<ExitStatus> for CommandResult {
     fn from(value: ExitStatus) -> Self {
         Self {
-            status_code: value.code().unwrap_or_default(),
+            status_code: value.code(),
             ..Default::default()
         }
     }
